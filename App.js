ea8d2a76b3c334761a59316e057c70667faa6005
Axel O <olougounaaxel@gmail.com>
2023-02-04 23:51:08 +0100

    search bar

--- App.js
+++ App.js
@@ -1,21 +1,9 @@
 import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Search from './components/Search'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello !</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Search/>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
