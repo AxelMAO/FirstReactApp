404957e60e8f0886861488a4783d3ba70afcddfa
Axel O <olougounaaxel@gmail.com>
2023-02-06 23:03:33 +0100

    test FlatList

--- components/Search.js
+++ components/Search.js
@@ -1,20 +1,30 @@
 // Componenents/Search.js
 
 import React from "react";
-import { StyleSheet, View, TextInput, Button} from 'react-native';
+import { StyleSheet, View, TextInput, Button, FlatList} from 'react-native';
 
 class Search extends React.Component{
     render(){
         return (
-            <View style={{marginTop: 20}}>
+            <View style={styles.main_container}>
               <TextInput style={styles.textinput} placeholder='Titre du film'/>
               <Button title='Rechercher' onPress={() => {}}/>
+
+            <FlatList
+                data={[{id: 'asce', title: '1 Film'}, {id: 'b', title: '2 Film'}]}
+                renderItem = {({item}) => <Text>{item.key}</Text>}
+            />    
             </View>
         )
     }
 }
 
 const styles = StyleSheet.create({
+    main_container: {
+        flex:1,
+        marginTop: 20
+    },
+    
     textinput: {
       marginLeft: 5,
       marginRight: 5,
