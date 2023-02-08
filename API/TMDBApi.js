6741b063e67c77b30b3f8938402709b98c6fccdd
Axel O <olougounaaxel@gmail.com>
2023-02-08 22:39:48 +0100

    Api film

---
+++ API/TMDBApi.js
@@ -0,0 +1,11 @@
+// API/TMDBApi.js
+
+const API_TOKEN = "2f1175c55eebdade96c958ceafa2458d";
+
+export function getFilmsFromApiWithSearchedText (text) {
+  const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text
+  return fetch(url)
+    .then((response)=W response.json())
+    .catch((error) => console.error(error))
+
+}
