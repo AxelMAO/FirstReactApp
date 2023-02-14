147e5a353a2c41ca63ec49ee8d163e8e2cc8dad5
Axel O <olougounaaxel@gmail.com>
2023-02-14 22:53:11 +0100

    minor changes

--- API/TMDBApi.js
+++ API/TMDBApi.js
@@ -13,3 +13,9 @@ export async function getFilmsFromApiWithSearchedText (text) {
   //  .then(response => response.json())
   //  .catch((error) => console.error(error))
 }
+
+// API/TMDBApi.js
+
+export function getImageFromApi (name) {
+  return 'https://image.tmdb.org/t/p/w300' + name
+}
